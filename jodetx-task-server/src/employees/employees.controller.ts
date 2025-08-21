69f474699f3ca5f8d4a77955d2dd1d6dbc538b66
amodmanjarekar <amodm.1102@gmail.com>
2025-08-21 19:49:19 +0530

    Add auth guard to employee routes

--- jodetx-task-server/src/employees/employees.controller.ts
+++ jodetx-task-server/src/employees/employees.controller.ts
@@ -1,27 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
+  @UseGuards(AuthGuard)
   @Post("new")
   create(@Body() createEmployeeDto: CreateEmployeeDto) {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @UseGuards(AuthGuard)
   @Post("bulk")
   bulkInsert(@Body() employees: CreateEmployeeDto[]) {
     return this.employeesService.bulkInsert(employees);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {[]
     return this.employeesService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.employeesService.remove(+id);
